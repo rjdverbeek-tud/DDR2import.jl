0ce1445e8f83ea8ee43b86c29d8618c1de076a2f
rjdverbeek <rjdverbeek@gmail.com>
2020-03-27 21:03:51 +0100

    Correct testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,14 +2,14 @@ using DDR2import
 using Test
 
 @testset "DDR2import.jl" begin
-    # tests = ["Implemented\\SO6", "Implemented\\T5", "Implemented\\Exp2",
-    # "Implemented\\Allftplus", "Implemented\\Ase", "Implemented\\Are",
-    # "Implemented\\Gar", "Implemented\\Gsl", "Implemented\\Frp",
-    # "Implemented\\Sls", "Implemented\\Sid", "Implemented\\Star",
-    # "Implemented\\Spc", "Implemented\\Mot", "Implemented\\Narp",
-    # "Implemented\\Nnpt", "Implemented\\Cost", "Implemented\\Crco",
-    # "Implemented\\Ur"]
-    tests = ["Implemented\\Arp"]
+    tests = ["Implemented\\SO6", "Implemented\\T5", "Implemented\\Exp2",
+    "Implemented\\Allftplus", "Implemented\\Ase", "Implemented\\Are",
+    "Implemented\\Gar", "Implemented\\Gsl", "Implemented\\Frp",
+    "Implemented\\Sls", "Implemented\\Sid", "Implemented\\Star",
+    "Implemented\\Spc", "Implemented\\Mot", "Implemented\\Narp",
+    "Implemented\\Nnpt", "Implemented\\Cost", "Implemented\\Crco",
+    "Implemented\\Ur", "Implemented\\Arp"]
+    # tests = ["Implemented\\Arp"]
 
     for t in tests
         include("$(t).jl")
